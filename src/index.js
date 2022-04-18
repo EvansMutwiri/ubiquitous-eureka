4bff64d65f3ee194ee9995e001ecc37e04ca470c
EvansMutwiri <mutwirievansm@gmail.com>
2022-04-18 22:30:54 +0300

    css loader

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import _ from 'lodash';
+import './style.css';
 
 function component() {
     const element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    
+    // add class called hello
+    element.classList.add('hello');
 
     return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,19 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+
+                // module loaders can be chained. A chain is executed in reverse order
+                // this order of loaders should be mantained
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
     },
 };
